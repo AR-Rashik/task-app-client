64a480c72e316d996bfaa8b489971d14a694f318
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-12-29 01:19:16 +0600

    navbar section added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -4,18 +4,15 @@ const Header = () => {
   const [show, setShow] = useState(false);
 
   return (
-    <div
-      className="bg-gray-100 pb-12 overflow-y-hidden"
-      style={{ minHeight: 700 }}
-    >
+    <div className="bg-gray-100 pb-12 overflow-y-hidden">
       {/* Code block starts */}
       <dh-component>
         <nav className="w-full border-b-2">
           <div className="py-5 md:py-0 container mx-auto px-6 flex items-center justify-between">
             <div aria-label="Home. logo" role="img">
               <img
-                className="w-12 md:w-auto"
-                src="https://tuk-cdn.s3.amazonaws.com/can-uploader/centre_aligned_simple-Svg1.svg"
+                className="w-auto h-6 lg:h-12 md:h-6 md:w-auto"
+                src="https://cdn-icons-png.flaticon.com/128/1069/1069189.png"
                 alt="logo"
               />
             </div>
@@ -71,20 +68,20 @@ const Header = () => {
                 </button>
                 <ul className="flex text-3xl md:text-base items-center py-10 md:flex flex-col md:flex-row justify-center fixed md:relative top-0 bottom-0 left-0 right-0 bg-white md:bg-transparent z-20">
                   <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0">
-                    <a href="/">Feature</a>
+                    <a href="/">Add Task</a>
                   </li>
-                  <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10">
-                    <a href="/">Marketplace</a>
+                  <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-10 lg:ml-20">
+                    <a href="/">My Task</a>
                   </li>
-                  <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10">
-                    <a href="/">Company</a>
+                  <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-10 lg:ml-20">
+                    <a href="/">Completed Task</a>
                   </li>
-                  <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10">
+                  {/* <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10">
                     <a href="/">Features</a>
                   </li>
                   <li className="text-gray-700 hover:text-gray-900 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10">
                     <a href="/">Contact</a>
-                  </li>
+                  </li> */}
                 </ul>
               </div>
             </div>
@@ -93,30 +90,6 @@ const Header = () => {
             </button>
           </div>
         </nav>
-        <div className="bg-gray-100">
-          <div className="container mx-auto flex flex-col items-center py-12 sm:py-24">
-            <div className="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10">
-              <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-gray-800 font-black leading-7 md:leading-10">
-                The Freedom to Create the
-                <span className="text-indigo-700">Websites</span>
-                You Want
-              </h1>
-              <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">
-                A professional website drives sales. Create a beautiful website
-                to impress and engage new customers and establish your business
-                online{" "}
-              </p>
-            </div>
-            <div className="flex justify-center items-center">
-              <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 bg-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 lg:text-xl lg:font-bold  rounded text-white px-4 sm:px-10 border border-indigo-700 py-2 sm:py-4 text-sm">
-                Get Started
-              </button>
-              <button className="ml-4 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 bg-transparent transition duration-150 ease-in-out hover:border-indigo-600 lg:text-xl lg:font-bold  hover:text-indigo-600 rounded border border-indigo-700 text-indigo-700 px-4 sm:px-10 py-2 sm:py-4 text-sm">
-                Live Demo
-              </button>
-            </div>
-          </div>
-        </div>
       </dh-component>
       {/* Code block ends */}
     </div>
