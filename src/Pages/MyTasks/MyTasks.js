883d913758a0ec1924521317b5962003d403c93a
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-12-29 20:19:39 +0600

    Login and signup page created

---
+++ src/Pages/MyTasks/MyTasks.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+
+const MyTasks = () => {
+  // const [showDropdown, setShowDropdown] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/tasks")
+      .then((res) => res.json())
+      .then((data) => setTasks(data));
+  }, []);
+
+  return (
+    <>
+      {/* Card is full width. Use in 8 col grid for best view. */}
+      {/* Card code block start */}
+      <div className="mx-auto bg-white dark:bg-gray-800 shadow rounded w-full">
+        <div className="py-6 xl:px-8 lg:px-8 md:px-8 px-4 flex justify-between items-center">
+          <p className="font-bold text-sm xl:text-lg lg:text-lg md:text-lg text-gray-800 dark:text-gray-100">
+            My All <span className="text-gray-500">Tasks</span>
+          </p>
+        </div>
+        {tasks.map((task) => (
+          <div className="xl:px-8 lg:px-8 md:px-8 px-4 py-6 bg-white dark:bg-gray-800">
+            <div className="p-5 flex justify-between rounded mb-6 bg-gray-100 dark:bg-gray-700">
+              <div className="w-3/5">
+                <p className="mb-2 text-sm font-bold text-gray-800 dark:text-gray-100">
+                  {task.title}
+                </p>
+                <ul>
+                  <li className="mb-2 text-xs text-gray-600 dark:text-gray-400">
+                    {task.description}
+                  </li>
+                </ul>
+              </div>
+              <div className="w-3/12 flex flex-col items-end justify-between">
+                <p className="text-xs text-gray-600 dark:text-gray-400">
+                  {task.time}
+                </p>
+                <div className="bg-gray-200 h-6 w-20 mb-4 md:mb-0 rounded-md flex items-center justify-center">
+                  <span className="text-xs text-gray-600 dark:text-gray-400 font-normal">
+                    Meeting
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* Card code block end */}
+    </>
+  );
+};
+
+export default MyTasks;
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,5 +1,10 @@
+import CompletedTask from "../../CompletedTask/CompletedTask";
 import Main from "../../Layout/Main";
+import AddTask from "../../Pages/AddTask/AddTask";
 import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import MyTasks from "../../Pages/MyTasks/MyTasks";
+import SignUp from "../../Pages/SignUp/SignUp";
 
 const { createBrowserRouter } = require("react-router-dom");
 
@@ -11,6 +16,26 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <SignUp></SignUp>,
+      },
+      {
+        path: "/mytasks",
+        element: <MyTasks></MyTasks>,
+      },
+      {
+        path: "/addtask",
+        element: <AddTask></AddTask>,
+      },
+      {
+        path: "/completedtask",
+        element: <CompletedTask></CompletedTask>,
       },
     ],
   },
